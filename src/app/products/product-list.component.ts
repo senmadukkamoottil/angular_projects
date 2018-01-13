95a4c16c6f3c1bb1b37fa05fd905b3b31a573986
Sen M <sentvla@gmail.com>
2018-01-12 21:05:55 -0800

    interface for product

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Iproduct } from './product';
 
 @Component({
     selector: 'pm-products',
@@ -8,14 +9,15 @@ export class ProductListComponent{
     public pageTitle = 'Product Listing';
     imageHeight: number = 30;
     showImage: boolean = true;
-    products: any[] = [
+    filterText: string = 'cart';
+    products: Iproduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
             "productCode": "GDN-0011",
             "releaseDate": "March 19, 2016",
             "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
+            "price": 19.9567,
             "starRating": 3.2,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
         },
@@ -25,7 +27,7 @@ export class ProductListComponent{
             "productCode": "GDN-0023",
             "releaseDate": "March 18, 2016",
             "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
+            "price": 32.9955,
             "starRating": 4.2,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
@@ -34,6 +36,6 @@ export class ProductListComponent{
         this.showImage = !this.showImage;
     };
     toggleButton(): void{
-        
+
     };
 }
